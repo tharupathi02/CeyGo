0d589913baef420493673b6f1bb1e7d5290ad21f
Deshan Tharupathi <tharupathi02@gmail.com>
2025-09-14 20:40:44 +0530

    feat: add primary and secondary color palettes to Tailwind config

---
+++ constant/Assets.ts
@@ -0,0 +1,21 @@
+const Assets = {
+  logo: {
+    blueBus: require('../assets/logo/BlueBus.png'),
+    blueLogo: require('../assets/logo/BlueLogo.png'),
+    blueSquareLogo: require('../assets/logo/BlueSquareLogo.png'),
+    whiteBus: require('../assets/logo/WhiteBus.png'),
+    whiteLogo: require('../assets/logo/WhiteLogo.png'),
+    whiteSquareLogo: require('../assets/logo/WhiteSquareLogo.png'),
+  },
+  appLogo: {
+    adaptiveIcon: require('../assets/app-logo/adaptive-icon.png'),
+    iosDark: require('../assets/app-logo/ios-dark.png'),
+    iosLight: require('../assets/app-logo/ios-light.png'),
+  },
+  images: {
+    androidIconForeground: require('../assets/app-logo/adaptive-icon.png'),
+    androidIconMonochrome: require('../assets/app-logo/adaptive-icon.png'),
+  },
+};
+
+export default Assets;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#5E2BFF',
+          50: '#F3EFFF',
+          100: '#E8DEFF',
+          200: '#D1BDFF',
+          300: '#BA9CFF',
+          400: '#A37BFF',
+          500: '#5E2BFF',
+          600: '#4B22CC',
+          700: '#381A99',
+          800: '#251166',
+          900: '#120933',
+        },
+        secondary: {
+          DEFAULT: '#FFC107',
+          50: '#FFF8E1',
+          100: '#FFECB3',
+          200: '#FFE082',
+          300: '#FFD54F',
+          400: '#FFCA28',
+          500: '#FFC107',
+          600: '#FFB300',
+          700: '#FF8F00',
+          800: '#FF6F00',
+          900: '#E65100',
+        },
+      },
+    },
   },
   plugins: [],
 }
